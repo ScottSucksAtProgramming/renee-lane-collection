d33e7d021814c2ca1cb9552514879f8d7152f97a
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-05-03 22:25:45 -0400

    Import metadata template from previous project

---
+++ back-end/metadata/metadata_template.py
@@ -0,0 +1,20 @@
+# ------------------------------ Documentation ------------------------------ #
+# Module:  sample_metadata.py
+# Containes the template for the metadata of this project.
+#
+#
+# Modification History
+# 04-18-2022 SRK Project Created
+
+# -------------------------------- Tasks ----------------------------------- #
+
+# ------------------------------- Resources -------------------------------- #
+metadata_template = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [{"trait_Type": "cuteness", "value": 100}],
+}
+# ------------------------------ Functions --------------------------------- #
+
+# ------------------------------ PythonHeader ------------------------------ #
