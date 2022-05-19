78603fdef01d7e5866b040e447f1cfa1fff767bb
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-05-19 18:57:41 -0400

    Add payouts and update to version 0.2.0 alpha
    # Version 0.2.0 alpha Changes
    Very happy to release RenneLaneCollection.sol version 0.2.0 alpha.
    
    This version supports minting and royalty payouts and payout
    splitting via the use of MoneyPipe (MoneyPipe.xyz) contracts. Minting
    payouts are made automatically upon successful minting of a token.
    Royalty information is created per token and stored as part of the
    token following the ERC-2981 standard. When a royalty payment is made
    it is directed to a MoneyPipe Contract which splits the amount and
    disperses the funds immediately.
    
    !! Please note that the ERC-2981 Royalty Standard is a declaration of
    preferences. It does not guarantee royalty payments will be made,
    this relies on a market places' honoring of the standard. OpenSea
    does not currently honor this standard, but the MoneyPipe Contract
    Address and be added manually to OpenSea as well other marketplaces.
    It is highly recommended you do this on all marketplaces you expect
    your NFTs to be sold. !!
    
    ## Minting and Royalty Payouts and Splits
    MoneyPipe Contracts handle the splits. Take a look at the design doc
    (https://bit.ly/rl_collab) for rational and additional information.
    
    MoneyPipe Contracts needs to be set up separately at (MoneyPipe.xyz).
    The design doc (https://bit.ly/rl_collab) has more information.
    The Minting Payout Split Contract will send 10% to the Artist and 90%
    to the Project Wallet. The Royalty Payout Split Contract will do a
    50/50 split between the artist and the Project Wallet.
    
    ### Image Struct Changes
    The Image Struct has two new variables attached (address
    mintingPayoutAddress and address royaltyPayoutAddress) where the
    MoneyPipe Contract addresses can be set during the constructor.
    
    !! The current addresses are dummy addresses and will not work.
    !! Change them for your deployment or your Ether will be lost.
    
    ## Minting Function Changes
    mintImage() now sets the royaltyPayoutAddress from the Image Struct.
    It will also send the msg.value paid for minting the image to the
    mintingPayoutAddress. The docstring for mintImage() was updated to
    include the new functionality.
    
    ## Assistant Functions Added
    These functions allow for information to be pulled from the contract
    for testing and verification purposes. Unlikely these will make it to
    the production version but they are helpful during development.
    getImageInfo() will return information based on an _imageNumber.
    getContractStoredBalance() returns the amount of Ether stored inside
    the contract. This should usually be zero.
    
    ## Documentation Changes
    The Statistics, Modification History and Task List were updated for
    the current version.

--- back-end/contracts/ReneeLaneCollection.sol
+++ back-end/contracts/ReneeLaneCollection.sol
@@ -6,10 +6,10 @@
 // originally written as a test contract used in the Scott's Puppy Mill
 // project. It is being repurposed and expanded upon for use in the Renee Lane Collection.
 //
-// Current Statistics as of 0.1.0 alpha
-// Deployment Cost  |   4,407,964 Gas
-// 1st Minting Cost |     101,137 Gas
-// Additional Mints |      84,037 Gas
+// Current Statistics as of 0.2.0 Alpha - Optmizer: 1000 Runs
+// Deployment Cost  |   6,165,943 Gas   | Approx: $370.00
+// 1st Minting Cost |     127,008 Gas   | Approx:   $7.62
+// Additional Mints |     107,343 Gas   | Approx:   $6.44
 //
 //
 // Modification History
@@ -19,17 +19,19 @@
 // 05-09-2022 | SRK | Minting Function Completed.
 // 05-10-2022 | SRK | Contract Ownership Functionality Added.
 // 05-15-2022 | SRK | Royalty Functionality Added.
-// 05-15-2022 | SRK | Version 0.1.0 alpha created.
+// 05-15-2022 | SRK | Version 0.1.0 Alpha released.
 // 05-18-2022 | SRK | mintImage() gas optimization pass.
+// 05-19-2022 | SRK | Implemented Minting and Royalty Payouts.
+// 05-19-2022 | SRK | Version 0.2.0 Alpha released.
 
 //* ------------------------------- Tasks --------------------------------- //
 // Update minting functions and counters to model the collection. - Complete (05/09/2022)
 // Add access control support. - Complete (05/10/2022)
 // Add royalty support. - Complete (05/15/2022)
-// Todo: Add Withdraw Payment functionality.
-// Todo: Implement Minting Payment Splits
-// Todo: Implement Royalty Payment Splits
-//      Todo: Create/Add MoneyPipe Contract Address for Royalty Splits
+// Todo: Add Withdraw Payment functionality. - Complete (5/19/2022)
+// Todo: Implement Minting Payment Splits.  - Complete (5/19/2022)
+// Todo: Implement Royalty Payment Splits.  - Complete (5/19/2022)
+// Todo: Create/Add MoneyPipe Contract Address for Royalty Splits.  - Complete (5/19/2022)
 // Todo: Update _baseURI to new IPFS metdata address.
 // Todo: Add Investor List functionality.
 // Todo: Gas Optimization Passes.
@@ -52,10 +54,10 @@ contract ReneeLaneCollection is ERC721, ERC721Royalty, Ownable {
         uint64 price;
         uint64 currentTokenId;
         uint64 lastTokenId;
+        address mintingPayoutAddress;
+        address royaltyPayoutAddress;
     }
 
-    //! This will be updated when we get actual moneypipe contracts set.
-    address dummyMoneypipeAddress = 0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC;
     //* ---------------------------- Mappings ----------------------------- //
     // Stores Image objects for each image by imageNumber.
     mapping(uint256 => Image) imageGallery;
@@ -66,13 +68,24 @@ contract ReneeLaneCollection is ERC721, ERC721Royalty, Ownable {
     constructor() ERC721("The Renee Lane Collection", "TRLC") {
         // Initializes Image Struct Objects. I couldn't come up with a
         // better way to do this math. This works.
-        imageGallery[1] = Image(1, 120000000000000000, 1, 20);
+        /** ! All of these addresses must be changed for the production version 
+        or your Ether will be lost. */
+        imageGallery[1] = Image(
+            1,
+            120000000000000000,
+            1,
+            20,
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+        );
         for (uint64 index = 2; index <= 10; index++) {
             imageGallery[index] = Image({
                 imgNumber: index,
                 price: 120000000000000000,
                 currentTokenId: imageGallery[index - 1].currentTokenId + 20,
-                lastTokenId: imageGallery[index - 1].lastTokenId + 20
+                lastTokenId: imageGallery[index - 1].lastTokenId + 20,
+                mintingPayoutAddress: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+                royaltyPayoutAddress: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
             });
         }
         for (uint64 index = 11; index <= 20; index++) {
@@ -80,34 +93,63 @@ contract ReneeLaneCollection is ERC721, ERC721Royalty, Ownable {
                 imgNumber: index,
                 price: 240000000000000000,
                 currentTokenId: imageGallery[index - 1].currentTokenId + 20,
-                lastTokenId: imageGallery[index - 1].lastTokenId + 20
+                lastTokenId: imageGallery[index - 1].lastTokenId + 20,
+                mintingPayoutAddress: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,
+                royaltyPayoutAddress: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
             });
         }
-        imageGallery[21] = Image(21, 360000000000000000, 401, 410);
+        imageGallery[21] = Image(
+            21,
+            360000000000000000,
+            401,
+            410,
+            0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,
+            0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+        );
         for (uint64 index = 22; index <= 30; index++) {
             imageGallery[index] = Image({
                 imgNumber: index,
                 price: 360000000000000000,
                 currentTokenId: imageGallery[index - 1].currentTokenId + 10,
-                lastTokenId: imageGallery[index - 1].lastTokenId + 10
+                lastTokenId: imageGallery[index - 1].lastTokenId + 10,
+                mintingPayoutAddress: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,
+                royaltyPayoutAddress: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
             });
         }
-        imageGallery[31] = Image(31, 480000000000000000, 501, 505);
+        imageGallery[31] = Image(
+            31,
+            480000000000000000,
+            501,
+            505,
+            0x617F2E2fD72FD9D5503197092aC168c91465E7f2,
+            0x617F2E2fD72FD9D5503197092aC168c91465E7f2
+        );
         for (uint64 index = 32; index <= 40; index++) {
             imageGallery[index] = Image({
                 imgNumber: index,
                 price: 480000000000000000,
                 currentTokenId: imageGallery[index - 1].currentTokenId + 5,
-                lastTokenId: imageGallery[index - 1].lastTokenId + 5
+                lastTokenId: imageGallery[index - 1].lastTokenId + 5,
+                mintingPayoutAddress: 0x617F2E2fD72FD9D5503197092aC168c91465E7f2,
+                royaltyPayoutAddress: 0x617F2E2fD72FD9D5503197092aC168c91465E7f2
             });
         }
-        imageGallery[41] = Image(41, 600000000000000000, 551, 553);
+        imageGallery[41] = Image(
+            41,
+            600000000000000000,
+            551,
+            553,
+            0x17F6AD8Ef982297579C203069C1DbfFE4348c372,
+            0x17F6AD8Ef982297579C203069C1DbfFE4348c372
+        );
         for (uint64 index = 42; index <= 50; index++) {
             imageGallery[index] = Image({
                 imgNumber: index,
                 price: 600000000000000000,
                 currentTokenId: imageGallery[index - 1].currentTokenId + 3,
-                lastTokenId: imageGallery[index - 1].lastTokenId + 3
+                lastTokenId: imageGallery[index - 1].lastTokenId + 3,
+                mintingPayoutAddress: 0x17F6AD8Ef982297579C203069C1DbfFE4348c372,
+                royaltyPayoutAddress: 0x17F6AD8Ef982297579C203069C1DbfFE4348c372
             });
         }
     }
@@ -116,9 +158,17 @@ contract ReneeLaneCollection is ERC721, ERC721Royalty, Ownable {
 
     /**
         This function will mint a token of the specified image passed to it. 
-        Will revert if there are no tokens left for that image, if the 
-        imageNumber is out of range or if the amount of Ether sent is less 
-        than the price of the image.
+        It wll revert if the image number is out side of this collection, if 
+        there are no tokens left for that image, or if the minter failed to 
+        send enough ether to cover the price.
+
+        Following the minting the royalty information is set via the 
+        _setTokenRoyalty() function inherited from OpenZeppelin's ERC721Royalty 
+        extension.
+
+        Finally, The price of the image is then transferred to a MoneyPipe 
+        contract which pays out 90% to the project and a 10% commision back to 
+        the artist.
 
         Arguments:
             - _imageNumber - The number of the image the user wants to mint (1-50).
@@ -138,8 +188,49 @@ contract ReneeLaneCollection is ERC721, ERC721Royalty, Ownable {
             "You didn't send enough Ether."
         );
         _safeMint(msg.sender, _newTokenId);
-        _setTokenRoyalty(_newTokenId, dummyMoneypipeAddress, 1000);
+        _setTokenRoyalty(
+            _newTokenId,
+            imageGallery[_imageNumber].royaltyPayoutAddress,
+            1000
+        );
+        payable(imageGallery[_imageNumber].mintingPayoutAddress).transfer(
+            msg.value
+        );
         imageGallery[_imageNumber].currentTokenId = _newTokenId + 1;
+    }
+
+    //* ------------------------ Assistant Functions -------------------------- //
+    // Various functions used for testing. Likely to be removed later.
+
+    function getImageInfo(uint256 _imageNumber)
+        public
+        view
+        returns (
+            uint64 imgNumber,
+            uint64 price,
+            uint64 currentTokenId,
+            uint64 lastTokenId,
+            address mintingPayoutAddress,
+            address royaltyPayoutAddress
+        )
+    {
+        return (
+            imageGallery[_imageNumber].imgNumber,
+            imageGallery[_imageNumber].price,
+            imageGallery[_imageNumber].currentTokenId,
+            imageGallery[_imageNumber].lastTokenId,
+            imageGallery[_imageNumber].mintingPayoutAddress,
+            imageGallery[_imageNumber].royaltyPayoutAddress
+        );
+    }
+
+    function getContractStoredBalance()
+        public
+        view
+        onlyOwner
+        returns (uint256 ContractBalance)
+    {
+        return address(this).balance;
     }
 
     //* ----------------------- Royalty Functions ------------------------- //
