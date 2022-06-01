b21d4da0008789c05219c0d120fe891c35bc0e82
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-05-31 20:32:41 -0400

    Create unit testing .py file and set tasks

---
+++ back-end/tests/unit/unit_tests.py
@@ -0,0 +1,44 @@
+# ------------------------------ Documentation ------------------------------ #
+# Module:  unit_tests.py
+# This module contains all the unit tests for The Renee Lane Collection smart
+# contracts.
+#
+#
+# Modification History
+# 05-31-22 | SRK | Module Created
+
+# -------------------------------- Tasks ----------------------------------- #
+# TODO: Write test to ensure functions of ERC721 are available on this contract.
+# TODO: Write test to ensure functions of ERC721Royalty are available on this contract.
+# TODO: Write test to ensure functions of Ownble are available on this contract.
+# TODO: Write a test that confirms the collection `name` is set correctly.
+# TODO: Write a test that confirms that the collection `symbol` is set correctly.
+# TODO: Write a test that confirms that the `imgNumber` is set correctly for all tokens.
+# TODO: Write a test that confirms that the `price` is set correctly for all tokens.
+# TODO: Write a test that confirms that the `currentTokenId` is set correctly for all tokens.
+# TODO: Write a test that confirms that the `lastTokenId` is set correctly for all tokens.
+# TODO: Write a test that confirms that the `mintingPayoutAddress` is set correctly for all tokens.
+# TODO: Write a test that confirms that the `royaltyPayoutAddress` is set correctly for all tokens.
+# TODO: Write a test that confirms the number passed to the function is the number that is used to for minting. (This is probably better done as an integration test with the frontend.)
+# TODO: Write a test to confirm the minting function will revert if the image number passed is out side of the range (1-50).
+# TODO: Write a test that confirms that `imageGallery[_imageNumber].currentTokenId` and `_newTokenId` are the same.
+# TODO: Write a test that confirms that the minting function will revert  if `_newTokenId` is greater than `imageGallery[_imageNumber].lastTokenId`.
+# TODO: Write a test that confirms that the minting function will revert if `msg.value` is less than `imageGallery[_imageNumber].price`.
+# TODO: Write a function that confirms that the minting function mints the correct image.
+# TODO: Write a test that confirms the minting function assigns the correct tokenId.
+# TODO: Write a test that confirms the minted token has the correct `imageGallery[_imageNumber].royaltyPayoutAddress`.
+# TODO: Write a test that confirms the minted token has the correct basis points for royalty split.
+# TODO: Write a test that confirms that after minting `imageGallery[_imageNumber].currentTokenId` has been correctly incremented by 1.
+# TODO: Write a test that confirms that the `_burn` function burns the correct token.
+# TODO: Write a test that confirms that the correct token's royalty information is erased after burning.
+# TODO: Write a test that confirms that `_baseURI` returns the expected address.
+# TODO: Write a test that confirms once a token is minted that the metadata for that token is created correctly.
+
+
+# ------------------------------- Resources -------------------------------- #
+
+# ------------------------------- Variables -------------------------------- #
+
+# ------------------------------ Functions --------------------------------- #
+
+# ----------------------------- Main Function ------------------------------ #
