0bccd104495426b1d9371156838e3a8438a57dec
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-07-02 20:17:53 -0400

    Update to 0.0.0 beta

--- back-end/metadata/metadata_template.py
+++ back-end/metadata/metadata_template.py
@@ -9,16 +9,51 @@
 # 05-03-2022 | SRK | Module Imported to Project.
 
 # -------------------------------- Tasks ----------------------------------- #
-
+# Todo: Create Field for Print Name,
+# Todo: Create Field for Description,
+# Todo: Create Field for Image URI,
+# Todo: Create Property for Royalty Fee and Royalty Address
+# Todo: Create Property for
+# Todo: Create Property for Artist,
+# Todo: Create Property for Medium
+# Todo: Create Property for Photo Source
+# Todo: Create Property for Title
+# Todo: Create Property for Edition
 # ------------------------------- Resources -------------------------------- #
 metadata_template = {
-    "name": "",
+
+    # Name of the item.
+    "name": "",                                         # Task 1
+
+    # A human readable description of the item. Markdown is supported.
     "description": "",
+
+    # This is the URL that will appear below the asset's image on OpenSea and
+    # will allow users to leave OpenSea and view the item on your site.
     "external_url": "",
+
+    # This is the URL to the image of the item. Can be just about any type of
+    # image (including SVGs, which will be cached into PNGs by OpenSea), and
+    # can be IPFS URLs or paths. We recommend using a 350 x 350 image.
     "image": "",
+
+    # These are the attributes for the item, which will show up on the OpenSea
+    # page for the item. (see below)
     "attributes": [
         {
             "trait_type": "Artist",
+            "value": "",
+        },
+        {
+            "trait_name": "Medium",
+            "value": "",
+        },
+        {
+            "trait_name": "Photo Source",
+            "value": "",
+        },
+        {
+            "trait_name": "Title",
             "value": "",
         },
         {
@@ -28,5 +63,7 @@ metadata_template = {
             "max_value": "",
         },
     ],
+    "Royalty Percentage": "10%",
+    "Royalty Address": "",
 }
 # ------------------------------ Functions --------------------------------- #
