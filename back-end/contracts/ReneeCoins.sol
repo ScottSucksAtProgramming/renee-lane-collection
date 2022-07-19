fcf7a13421280fa4235eeb32addb553ad3bc7ad8
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-07-18 21:45:53 -0400

    Remove unnecessary comments from contract

--- back-end/contracts/ReneeCoins.sol
+++ back-end/contracts/ReneeCoins.sol
@@ -35,55 +35,13 @@
  * @title Renee Coins
  * @author Scott Kostolni
  *
- * @notice Version 0.0.0 beta
+ * @dev Version 0.0.0 beta
  *
- * @notice This is a bespoke ERC-20 Contract that provides the social
- * currency, Renee Coins, which grants benefits to investors and artists of
- * The Renee Lane NFT Collection.
- *
- * @notice This contract acts as the ledger for the Renee Coins and allows
- * for Renee Coins to be created, tracked, and transferred or traded between
- * wallets. It uses the OpenZeppelin contracts as a back bone with extension
- * allowing a maximum cap to be specified, and for the owner to destroy or
- * burn their tokens. The OpenZepplin Ownable extension is used to set an
- * Owner for the contract and ensure certain functions are ONLY callable by
- * the Owner.
- */
-
-//* ------------------------- Modification History ------------------------ //
-/**
- * 06-26-2022 | SRK | Contract deployed on Rinkeby: 0xbfC39dF8CD38Cb19175650AB1B1ABcD18C8edF6b
- * 07-01-2022 | SRK | Contract statistics and documetation updated for beta release.
- */
-
-//* ------------------------------ Statistics ----------------------------- //
-/**
- * @notice Current Gas Useage as of 0.0.0 Beta - Optmizer: 1,000 Runs
- * ReneeCoins <Contract>
- *  ├─ deployment             -  avg: 1347574  avg (confirmed): 1347574  low: 1347574  high: 1347574  USD: $18.52
- *  ├─ constructor            -  avg: 1063242  avg (confirmed): 1063242  low: 1063242  high: 1063242  USD: $14.61
- *  ├─ createCoins            -  avg:   65546  avg (confirmed):   67987  low:   22422  high:   68009  USD:  $0.93
- *  ├─ airdropCoins           -  avg:   45935  avg (confirmed):   68490  low:   22923  high:   68490  USD:  $0.94
- *  ├─ approve                -  avg:   42875  avg (confirmed):   44108  low:   21925  high:   44120  USD:  $0.61
- *  ├─ transfer               -  avg:   41514  avg (confirmed):   51009  low:   21946  high:   51009  USD:  $0.70
- *  ├─ transferFrom           -  avg:   39996  avg (confirmed):   45152  low:   23621  high:   59675  USD:  $0.82
- *  ├─ burn                   -  avg:   32805  avg (confirmed):   35354  low:   22613  high:   35354  USD:  $0.49
- *  ├─ increaseAllowance      -  avg:   30173  avg (confirmed):   30173  low:   30173  high:   30173  USD:  $0.41
- *  ├─ decreaseAllowance      -  avg:   30106  avg (confirmed):   30106  low:   30106  high:   30106  USD:  $0.41
- *  ├─ burnFrom               -  avg:   27770  avg (confirmed):   28942  low:   23089  high:   28942  USD:  $0.40
- *  ├─ transferOwnership      -  avg:   26444  avg (confirmed):   30117  low:   22772  high:   30117  USD:  $0.41
- *  ├─ symbol                 -  avg:   24452  avg (confirmed):   24452  low:   24452  high:   24452  USD:  $0.34
- *  ├─ name                   -  avg:   24387  avg (confirmed):   24387  low:   24387  high:   24387  USD:  $0.32
- *  ├─ allowance              -  avg:   23258  avg (confirmed):   23258  low:   23258  high:   23258  USD:  $0.31
- *  ├─ balanceOf              -  avg:   22715  avg (confirmed):   22715  low:   22706  high:   22718  USD:  $0.30
- *  ├─ owner                  -  avg:   22132  avg (confirmed):   22132  low:   22132  high:   22132  USD:  $0.30
- *  ├─ totalSupply            -  avg:   22113  avg (confirmed):   22113  low:   22113  high:   22113  USD:  $0.29
- *  ├─ decimals               -  avg:   21309  avg (confirmed):   21309  low:   21309  high:   21309  USD:  $0.29
- *  ├─ cap                    -  avg:   21301  avg (confirmed):   21301  low:   21301  high:   21301  USD:  $0.29
- *  └─ renounceOwnership      -  avg:   18496  avg (confirmed):   14767  low:   14767  high:   22226  USD:  $0.31
- * Note: USD Calculations based on Gas Price:13 Wei and Ethereum price: $1057 from 07-01-2022.
- * Formula: TransactionCost =  (Gas (High) * Gas Price * Etherum USD Price) / 1,000,000,000
- */
+ * @dev This is a bespoke ERC-20 Contract that provides the social currency, 
+ * Renee Coins, which grants benefits to investors and artists of The Renee 
+ * Lane NFT Collection. For more information, see the README.md file located 
+ * at: https://bit.ly/rl_readme, as well as additional documentation located 
+ * at: https://bit.ly/rl_docs.
 
 //* ------------------------------- Tasks --------------------------------- //
 /**
@@ -98,94 +56,20 @@
  * Todo: Celebrate?!
  */
 
-//* ----------------------------- Resources ------------------------------- //
-/**
- * @notice Pragma statements tell the compiler to use the version of
- * solidity this contract was designed for.
- */
 pragma solidity 0.8.15;
 
-/**
- * @notice Import statements allow the contract to access features of
- * other contracts. Specifically OpenZeppelin's ERC20, ERC20Burnable,
- * ERC20Capped, and Ownable libraries.
- */
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//* ----------------------------- Contract -------------------------------- //
-
-/**
- * @notice The contract statement defines the contract's name, and the
- * libraries that it uses.
- *
- * @notice The ReneeCoin Contract inherits OpenZeppelin's ERC20,
- * ERC20Burnable, ERC20Capped and Ownable extenions to ensure compliance to
- * the current standards and provide a strong security basis with code that
- * has been vetted elsewhere.
- */
 contract ReneeCoins is ERC20, ERC20Burnable, ERC20Capped, Ownable {
-    //* ------------------------ Data Structures -------------------------- //
-    /**
-     * @notice No data structures created for this contract.
-     */
-    //* --------------------------- Constructor --------------------------- //
-    /**
-     * @notice The constructor helps set up the initial state of the smart
-     * contract. It is called when the contract is deployed. In the
-     * constructor the total number of Renee Coins which can be in circulation
-     * at any given time is capped at 2,000,000. This cap is only set during
-     * the deployment of this contract and cannot be changed later.
-     *
-     * @notice The name of the ERC-20 Token and Symbol are also permanently
-     * set in the constructor.
-     */
     constructor() ERC20("Renee Coins", "RC") ERC20Capped(2000000) {}
 
-    //* ----------------------- External Functions ------------------------ //
-    /**
-     * @notice External functions can ONLY be called from other contracts, or
-     * users.
-     */
-
-    //* -------------------- OnlyOwner Functions -------------------------- //
-    /**
-     * @notice OnlyOwner functions are only callable by the owner of the
-     * contract. They are subclass of External Functions.
-     */
-
-    /**
-     * @notice When this contract is initially deployed, there are 0 Renee
-     * Coins in existence. New Renee Coins must be 'minted' in order to
-     * create them. createCoins() serves as the mintingfunction for this
-     * contract. It can be called only by the owner and allows them to mint
-     * as many Renee Coins as they like, up to the cap.
-     *
-     * This function will ONLY create coins in the Owner's wallet. The
-     * airdropCoins() function can be used to mint coins directly to someone
-     * else's wallet.
-     *
-     * @param amountOfCoinsToCreate The number of coins that are to be minted.
-     */
     function createCoins(uint256 amountOfCoinsToCreate) external onlyOwner {
         _mint(msg.sender, amountOfCoinsToCreate);
     }
 
-    /**
-     * @notice The airdropCoins() function allows the Owner to mint new Renee
-     * Coins directly into a specified wallet address. This is significantly
-     * cheaper than minting and transferring the coins separately.
-     *
-     * This function can ONLY be called by the Owner.
-     *
-     * @param recipientAddress The wallet address of the recipient of the
-     * airdrop.
-     *
-     * @param amountOfCoinsToAirdrop The number of coins that are to be
-     * created.
-     */
     function airdropCoins(
         address recipientAddress,
         uint256 amountOfCoinsToAirdrop
@@ -193,22 +77,6 @@ contract ReneeCoins is ERC20, ERC20Burnable, ERC20Capped, Ownable {
         _mint(recipientAddress, amountOfCoinsToAirdrop);
     }
 
-    //* ----------------------- Public Functions -------------------------- //
-    /**
-     * @notice Public functions are callable by anyone, external or internal.
-     *
-     * @notice There are no public functions in this contract.
-     */
-
-    /**
-     * @notice The decimals() function replaces the inherited functions from
-     * the ERC20 and ERC20Capped OpenZeppelin Contracts. For Renee Coins
-     * there are 0 decimal places, these tokens cannot be broken down in
-     * smaller increments. This function is required by the ERC-20 standard.
-     *
-     * @return decimalPlaces The amount of decimal places that are supported
-     * by these tokens.
-     */
     function decimals()
         public
         view
@@ -219,23 +87,6 @@ contract ReneeCoins is ERC20, ERC20Burnable, ERC20Capped, Ownable {
         return 0;
     }
 
-    //* ----------------------- Internal Functions ------------------------ //
-    /**
-     * @notice Internal functions can ONLY be called by this contract itself.
-     */
-
-    /**
-     * @notice The _mint() function replaces the inherited functions from
-     * the ERC20 and ERC20Capped Openzepplin Contracts. This is an internal
-     * function which can only be called by the contract itself and is used
-     * inside other functions such as createCoins() and airdropCoins().
-     *
-     * @param recipientAddress The wallet address to which coins will be
-     * minted.
-     *
-     * @param amountOfCoinsToCreate The number of coins that are to be
-     * created.
-     */
     function _mint(address recipientAddress, uint256 amountOfCoinsToCreate)
         internal
         virtual
